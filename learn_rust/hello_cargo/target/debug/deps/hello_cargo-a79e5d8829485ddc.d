68ce466a6f1d63f1685673afb35410a7e3e47394
agao <allengaohs@gmail.com>
2023-05-01 20:42:57 -0400

    learning rust

---
+++ learn_rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/agao/personal/projects/crumbs/learn_rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/agao/personal/projects/crumbs/learn_rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ learn_rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/agao/personal/projects/crumbs/learn_rust/hello_cargo/target/debug/hello_cargo: /home/agao/personal/projects/crumbs/learn_rust/hello_cargo/src/main.rs
