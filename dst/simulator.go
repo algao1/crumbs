0fac0da8b0b4a94194f4b7ecbe97362c31f3bb19
agao <allengaohs@gmail.com>
2024-06-20 05:30:05 -0400

    update dst documentation

--- dst/simulator.go
+++ dst/simulator.go
@@ -6,6 +6,24 @@ import (
 	"runtime"
 )
 
+// This was made when I was going through my deterministic
+// state testing "phase". I wanted to try to implement DST
+// inside Go, but because of how the runtime works, it would
+// require intercepting syscalls much like Reverie.
+
+// Instead, I decided to write my own simulated environment
+// where I can make certain things deterministic, such as
+// goroutines yielding. This is done using coroutines, which
+// aren't implemented natively in Go, yet.
+
+// The basic idea is pretty simple, we have a simulator which
+// consists of a (random number) generator, a timer that simulates
+// time, and a scheduler that schedules events to be processed.
+// Everything is processed in a single thread, which is the simulator.
+
+// The scheduler is just a LIRO (last-in-random-out) queue, and
+// manually yielding inserts the event back into the queue.
+
 type Simulator struct {
 	Generator *Generator
 	Timer     *Timer
